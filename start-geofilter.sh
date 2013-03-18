653633f8d50ad27af3c23235d6721694d3db15fa
Boris Okner <boris.okner@gmail.com>
2013-03-17 19:15:49 -0700

    added -name and -setcookie to the script

--- start-geofilter.sh
+++ start-geofilter.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 cd `dirname $0`
-exec erl -pa $PWD/ebin $PWD/deps/*/ebin -boot start_sasl -s reloader -s geofilter
+exec erl -pa $PWD/ebin $PWD/deps/*/ebin -boot start_sasl -name erlocator -setcookie erlocator -s reloader -s geofilter
