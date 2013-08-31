a6badefd1dd02a16802179f6024caec1184c1af9
Boris Okner <boris.okner@gmail.com>
2013-08-31 04:06:21 +0000

    Initial commit for webrtc/jingle

---
+++ webrtc/strophe.jingle/README.md
@@ -0,0 +1,27 @@
+strophe.jingle
+==============
+
+strophe.jingle is a webrtc connection plugin for [strophe.js](http://strophe.im/strophejs/). Strophe is a popular library for writing XMPP client applications that run on any of the current popular browsers. Instead of the native TCP binding, strophe.js uses BOSH (Bidirectional-streams Over Synchronous HTTP, a variant of long polling) to connect to an XMPP server. Besides enabling anyone to build (federated) IM applications, this opens up the browser as an addressable endpoint for two-way exchange of structured messages, including presence and publish-subscribe applications.
+
+This plugin makes it possible to negotiate audio/video streams via XMPP and then relinquish control to the WebRTC support of browsers like Firefox and Chrome for the actual out-of-band media streams. XMPP/Jingle you get the authenticated, secured and federated media signaling, whereas WebRTC gives you an API to set up the media streams using RTP/ICE/STUN and provide access to cameras and microphones.
+
+Features:
+- mostly standards-compliant jingle, mapping from WebRTCs SDP to Jingle and vice versa. Aiming for full compliance with XEPs 0166, 0167, 0293, 0294 and 0320.
+- tested with chrome and firefox.
+- trickle and non-trickle modes for ICE (XEP-0176). Even supports early candidates from peer using PRANSWER.
+- support for fetching time-limited STUN/TURN credentials through XEP-0215. [rfc5766-turn-server](https://code.google.com/p/rfc5766-turn-server/) is a TURN server which implements this method.
+- a sample demonstrating the use of this to build a federated multi-user conference (in full-mesh mode). When [hark](https://github.com/latentflip/hark) is available, the local audio volume is visualized (in Chrome M29+).
+
+Events:
+- callincoming.jingle (sid)
+- callterminated.jingle (sid)
+- nostuncandidates.jingle (sid)
+- remotestreamadded.jingle (event, sid)
+- remotestreamremoved.jingle (event, sid)
+- iceconnectionstatechange.jingle (sid, session)
+- mediaready.jingle (stream)
+- mediafailure.jingle
+- ringing.jingle (sid)
+- mute.jingle (sid, content)
+- unmute.jingle (sid, content)
+- error.jingle (sid, error)
---
+++ webrtc/strophe.jingle/strophe.jingle.adapter.js
@@ -0,0 +1,126 @@
+// mozilla chrome compat layer -- very similar to adapter.js
+function setupRTC() {
+    var RTC = null;
+    if (navigator.mozGetUserMedia && mozRTCPeerConnection) {
+        console.log('This appears to be Firefox');
+        var version = parseInt(navigator.userAgent.match(/Firefox\/([0-9]+)\./)[1]);
+        if (version >= 22) {
+            RTC = {
+                peerconnection: mozRTCPeerConnection,
+                browser: 'firefox',
+                getUserMedia: navigator.mozGetUserMedia.bind(navigator),
+                attachMediaStream: function(element, stream) {
+                    element[0].mozSrcObject = stream;
+                    element[0].play();
+                },
+                pc_constraints: {}
+            };
+            if (!MediaStream.prototype.getVideoTracks)
+                MediaStream.prototype.getVideoTracks = function() { return []; };
+            if (!MediaStream.prototype.getAudioTracks)
+                MediaStream.prototype.getAudioTracks = function() { return []; };
+            RTCSessionDescription = mozRTCSessionDescription;
+            RTCIceCandidate = mozRTCIceCandidate;
+        }
+    } else if (navigator.webkitGetUserMedia) {
+        console.log('This appears to be Chrome');
+        RTC = {
+            peerconnection: webkitRTCPeerConnection,
+            browser: 'chrome',
+            getUserMedia: navigator.webkitGetUserMedia.bind(navigator),
+            attachMediaStream: function(element, stream) {
+                element.attr('src', webkitURL.createObjectURL(stream));
+            },
+//            pc_constraints: {} // FIVE-182
+            pc_constraints: {'optional': [{'DtlsSrtpKeyAgreement': 'true'}]} // enable dtls support in canary
+        };
+        if (navigator.userAgent.indexOf('Android') != -1) {
+            RTC.pc_constraints = {}; // disable DTLS on Android
+        }
+        if (!webkitMediaStream.prototype.getVideoTracks) {
+            webkitMediaStream.prototype.getVideoTracks = function()
+            { return this.videoTracks; };
+        }
+        if (!webkitMediaStream.prototype.getAudioTracks) {
+            webkitMediaStream.prototype.getAudioTracks = function()
+            { return this.audioTracks; };
+        }
+    }
+    if (RTC == null) {
+        try { console.log('Browser does not appear to be WebRTC-capable'); } catch (e) { }
+    }
+    return RTC;
+}
+
+function getUserMediaWithConstraints(um, resolution, bandwidth, fps) {
+
+    var constraints = {audio: false, video: false};
+
+    if ($.inArray('video', um) >= 0) {
+        constraints.video = true;
+    }
+    if ($.inArray('audio', um) >= 0) {
+        constraints.audio = true;
+    }
+    if ($.inArray('screen', um) >= 0) {
+        constraints.video = {
+            "mandatory": {
+                "chromeMediaSource": "screen"
+            }
+        }
+    }
+
+    // see https://code.google.com/p/chromium/issues/detail?id=143631#c9 for list of supported resolutions
+    switch (resolution) {
+        // 16:9 first
+        case '720':
+        case 'hd':
+            constraints.video = {mandatory: {minWidth: 1280, minHeight: 720, minAspectRatio: 1.77}};
+            break;
+        case '360':
+            constraints.video = {mandatory: {minWidth: 640, minHeight: 360, minAspectRatio: 1.77}};
+            break;
+        case '180':
+            constraints.video = {mandatory: {minWidth: 320, minHeight: 180, minAspectRatio: 1.77}};
+            break;
+            // 4:3
+        case '960':
+            constraints.video = {mandatory: {minWidth: 960, minHeight: 720}};
+            break;
+        case '640':
+        case 'vga':
+            constraints.video = {mandatory: {maxWidth: 640, maxHeight: 480}};
+            break;
+        case '320':
+            constraints.video = {mandatory: {maxWidth: 320, maxHeight: 240}};
+            break;
+        default:
+            if (navigator.userAgent.indexOf('Android') != -1) {
+                constraints.video = {mandatory: {maxWidth: 320, maxHeight: 240, maxFrameRate: 15}};
+            }
+            break;
+    }
+
+    if (bandwidth) { // doesn't work currently, see webrtc issue 1846
+        constraints.video.optional = [{bandwidth: bandwidth}];
+    }
+    if (fps) { // for some cameras it might be necessary to request 30fps
+        // so they choose 30fps mjpg over 10fps yuy2
+        constraints.video.mandatory['minFrameRate'] = fps;
+    }
+ 
+    try {
+        RTC.getUserMedia(constraints,
+                function(stream) {
+                    console.log('onUserMediaSuccess');
+                    $(document).trigger('mediaready.jingle', [stream]);
+                },
+                function(error) {
+                    console.warn('Failed to get access to local media. Error ', error);
+                    $(document).trigger('mediafailure.jingle');
+                });
+    } catch (e) {
+        console.error('GUM failed: ', e);
+        $(document).trigger('mediafailure.jingle');
+    }
+}
