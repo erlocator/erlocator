cd6bedd6428e04fbfa677cf0b4a36738f01d829b
Boris Okner <boris.okner@gmail.com>
2013-09-14 02:45:02 -0700

    shortcut to get to the desired location (specify lat and lon params in the url)

--- priv/www/geofilter.js
+++ priv/www/geofilter.js
@@ -618,3 +618,8 @@ function push_presence() {
                 });
 }
 
+//Get url param
+function getURLParameter(name) {
+    return decodeURIComponent((new RegExp('[?|&]' + name + '=' + '([^&;]+?)(&|#|;|$)').exec(location.search)||[,""])[1].replace(/\+/g, '%20'))||null;
+}
+
